66ab2677a266452e10898a8644d65d28c88b40b4
Tony Wu <knowbow@gmail.com>
2020-08-03 17:54:28 -0400

    event page

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -324,17 +324,23 @@ var Events = /*#__PURE__*/function (_React$Component) {
     key: "body",
     value: function body() {
       return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "events-body-container"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "events-body"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Climbing With Friends is climbing, with company"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "For two hours, you and company climb at a rock-climbing gym with a host to lead a session. The circumstances are unusual, but that's the point"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "If none of these work for you, you can sign up and we'll email you when next month's tea times are all up. You can also apply to be a Host!"));
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "Climbing With Friends is climbing, with company"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "For two hours, you and company climb at a rock-climbing gym with a host to lead a session. The circumstances are unusual, but that's the point"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "If none of these work for you, you can sign up and we'll email you when next month's tea times are all up. You can also apply to be a Host!")));
     }
   }, {
     key: "cityList",
     value: function cityList() {
       return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "city-list-container"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "city-list-title"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Jump to your city's climbing times!")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "city-list"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Jump to your city's climbing times!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "cities-list"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "San Francisco"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "New York City"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Washington DC"))));
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+        className: "cities"
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "San Francisco"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "New York City"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, "Washington DC"))));
     }
   }, {
     key: "citiesEvents",
@@ -374,7 +380,7 @@ var Events = /*#__PURE__*/function (_React$Component) {
       }];
       var renderCities = seed.map(function (city) {
         return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
-          className: "eventIndex-container",
+          className: "cities-container",
           key: city.city_name.length
         }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
           className: "city"
@@ -410,9 +416,7 @@ var Events = /*#__PURE__*/function (_React$Component) {
         className: "home",
         alt: "Friends",
         src: "https://images.squarespace-cdn.com/content/v1/5be4f2c0f793929686b0a305/1567669642144-HELFNPOTBA55RPC2J0FL/ke17ZwdGBToddI8pDm48kGPVK--wGoWXJsqwlxbZlQN7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5enfxu_O4VeONvneR-F6W8oeFhFqSrYyNrfPB9Y70_gvQ/LM_181130_ClimbFitKirrawee_0086_HIGHres.jpg?format=2500w"
-      })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "frontpage"
-      }, this.intro(), this.body(), this.cityList(), this.citiesEvents()));
+      })), this.intro(), this.body(), this.cityList(), this.citiesEvents());
     }
   }]);
 
--- frontend/components/events/events.jsx
+++ frontend/components/events/events.jsx
@@ -18,20 +18,24 @@ class Events extends React.Component {
 
     body(){
         return(
-            <div className="events-body">
-                <h3>Climbing With Friends is climbing, with company</h3>
-                <p>For two hours, you and company climb at a rock-climbing gym with a host to lead a session. The circumstances are unusual, but that's the point</p>
-                <p>If none of these work for you, you can sign up and we'll email you when next month's tea times are all up. You can also apply to be a Host!</p>
+            <div className="events-body-container">
+                <div className="events-body"> 
+                    <h3>Climbing With Friends is climbing, with company</h3>
+                    <p>For two hours, you and company climb at a rock-climbing gym with a host to lead a session. The circumstances are unusual, but that's the point</p>
+                    <p>If none of these work for you, you can sign up and we'll email you when next month's tea times are all up. You can also apply to be a Host!</p>
+                </div>
             </div>
         )
     }
 
     cityList(){
         return(
-            <div className="city-list">
+            <div className="city-list-container">
+                <div className="city-list-title">
                 <p>Jump to your city's climbing times!</p>
-                <div className='cities-list'>
-                    <ul>
+                </div>
+                <div className='city-list'>
+                    <ul className="cities">
                         <li>
                             San Francisco
                         </li>
@@ -89,7 +93,7 @@ class Events extends React.Component {
         ]
         const renderCities = seed.map(city => {
             return (
-                <ul className="eventIndex-container" key={city.city_name.length}>
+                <ul className="cities-container" key={city.city_name.length}>
                     <li className="city">
                         {city.city_name}
                         <span>👉🏽</span>
@@ -147,12 +151,12 @@ class Events extends React.Component {
                     <img className="home" alt="Friends" src="https://images.squarespace-cdn.com/content/v1/5be4f2c0f793929686b0a305/1567669642144-HELFNPOTBA55RPC2J0FL/ke17ZwdGBToddI8pDm48kGPVK--wGoWXJsqwlxbZlQN7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5enfxu_O4VeONvneR-F6W8oeFhFqSrYyNrfPB9Y70_gvQ/LM_181130_ClimbFitKirrawee_0086_HIGHres.jpg?format=2500w"
                     />
                 </div>
-                <div className="frontpage">
+                
                     {this.intro()}
                     {this.body()}
                     {this.cityList()}
                     {this.citiesEvents()}
-                </div>
+                
             </div>
         )
     }
